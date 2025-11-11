c16eb3aeeeffedab0ff1debd609cac6aa9cb7a23
Sebhvarg <sebhvarg@espol.edu.ec>
2025-11-10 23:58:55 -0500

    último commit

--- test/algoritmo1.rs
+++ test/algoritmo1.rs
@@ -1,9 +1,21 @@
 fn main() {
     print("Hello, world!");
-    let x = 5;
+    let x = 5
     let y = 10;
     let z = x + y - 3;
     lit e = x && y;
     print(x};
+    let o + 67;
     x = y && z ;
+    if x > y {
+        print("x is greater than y");
+    }  ()
+        print("x is not greater than y");
+    }
+
+    iff x > y {
+        print("x is greater than y");
+    } else {
+        print("x is not greater than y");
+    }
 }
